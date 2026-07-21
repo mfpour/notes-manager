79753724230f1e99ca4d01e3d2a1734fa9169d08
mfpour <flotkeh@gmail.com>
2026-07-21 11:48:00 +0330

    Implement course CRUD API

--- backend/courses/urls.py
+++ backend/courses/urls.py
@@ -1,4 +1,8 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-]
+from .views import CourseViewSet
+
+router = DefaultRouter()
+router.register("", CourseViewSet, basename="courses")
+
+urlpatterns = router.urls
--- backend/courses/views.py
+++ backend/courses/views.py
@@ -1,1 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Course
+from .serializers import CourseSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    serializer_class = CourseSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Course.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
