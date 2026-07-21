c27948d0bebedf2ef0ae5139791d3f03aea6b6fc
mfpour <flotkeh@gmail.com>
2026-07-21 12:29:00 +0330

    Finalize backend API implementation

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -35,7 +35,18 @@ def login_view(request):
 
     login(request, user)
 
-    return Response({"message": "Login successful"})
+    return Response(
+        {
+            "message": "Login successful",
+            "user": {
+                "id": user.id,
+                "username": user.username,
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+                "email": user.email,
+            },
+        }
+    )
 
 
 @api_view(["POST"])
--- backend/notes/views.py
+++ backend/notes/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
 
+from courses.models import Course
 from .models import Note
 from .serializers import NoteSerializer
 
@@ -11,3 +13,22 @@ class NoteViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return Note.objects.filter(course__owner=self.request.user)
+
+    def perform_create(self, serializer):
+        course = serializer.validated_data["course"]
+
+        if course.owner != self.request.user:
+            raise PermissionDenied("You do not have permission to use this course.")
+
+        serializer.save()
+
+    def perform_update(self, serializer):
+        course = serializer.validated_data.get(
+            "course",
+            serializer.instance.course,
+        )
+
+        if course.owner != self.request.user:
+            raise PermissionDenied("You do not have permission to use this course.")
+
+        serializer.save()
