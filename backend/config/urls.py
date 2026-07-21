1c5d1656f4ddbf6708fcded2c27c98813f620ee6
mfpour <flotkeh@gmail.com>
2026-07-21 11:02:00 +0330

    Configure application routes

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),
+    path("courses/", include("courses.urls")),
+    path("notes/", include("notes.urls")),
 ]
 
 if settings.DEBUG:
