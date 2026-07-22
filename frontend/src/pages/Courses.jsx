d30d35cccac31898f09770a26cf1d78473523525
mfpour <flotkeh@gmail.com>
2026-07-23 00:35:00 +0330

    Add courses page

--- frontend/src/pages/Courses.jsx
+++ frontend/src/pages/Courses.jsx
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react";
+import api from "../api/axios";
+
 function Courses() {
-  return <h1>Courses</h1>;
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    fetchCourses();
+  }, []);
+
+  const fetchCourses = async () => {
+    try {
+      const response = await api.get("/courses/");
+      setCourses(response.data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Courses</h1>
+
+      {courses.length === 0 ? (
+        <p>No courses found.</p>
+      ) : (
+        <ul>
+          {courses.map((course) => (
+            <li key={course.id}>
+              {course.name}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default Courses;
