52bca826d5f0c9716c9baf4b56af1ff04c3437a3
mfpour <flotkeh@gmail.com>
2026-07-23 20:18:00 +0330

    Add notes page

--- frontend/src/pages/Notes.jsx
+++ frontend/src/pages/Notes.jsx
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react";
+import api from "../api/axios";
+
 function Notes() {
-  return <h1>Notes</h1>;
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  const fetchNotes = async () => {
+    try {
+      const response = await api.get("/notes/");
+      setNotes(response.data);
+    } catch (error) {
+      console.log(error.response?.data);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Notes</h1>
+
+      {notes.length === 0 ? (
+        <p>No notes found.</p>
+      ) : (
+        <ul>
+          {notes.map((note) => (
+            <li key={note.id}>
+              <strong>{note.title}</strong>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default Notes;
